73db0f98437b9e82e733c301de078c420ce56e64
abinj <abinmj656@gmail.com>
2019-09-28 23:44:57 -0400

    Root mean sqaure response added

--- LinearRegression/linear_regression.py
+++ LinearRegression/linear_regression.py
@@ -1,30 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
-
-sns.set()
 
 from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv("LinearRegression/linear_regression_df.csv")
+df = pd.read_csv("dataset.csv")
 df.columns = ['X', 'Y']
-plt.scatter(x=df['X'], y=df['Y'])
-plt.ylabel('Response')
-plt.xlabel('Explanatory')
 
-linear = linear_model.LinearRegression()
 trainX = np.asarray(df.X[20:len(df.X)]).reshape(-1, 1)
 trainY = np.asarray(df.Y[20:len(df.Y)]).reshape(-1, 1)
 testX = np.asarray(df.X[:20]).reshape(-1, 1)
 testY = np.asarray(df.Y[:20]).reshape(-1, 1)
-linear.fit(trainX, trainY)
-r2_value = linear.score(trainX, trainY)
-print('Coefficient: \n', linear.coef_)
-print("Intercept: \n", linear.intercept_)
-print("R^2value: \n", r2_value)
-predicted = linear.predict(testX)
-print(predicted)
 
+# Model initialization
+model = linear_model.LinearRegression()
+# Fit the data
+model.fit(trainX, trainY)
+# Predict
+predicted = model.predict(testX)
+
+# Model evaluation
+r2_value = model.score(trainX, trainY)
+# Root Mean Squared Deviation
+rmsd = np.sqrt(mean_squared_error(testY, predicted))
+
+# Printing values
+print('Slope: \n', model.coef_)
+print("Root Mean Square Deviation: \n", rmsd)
+print("R^2 value: \n", r2_value)
+print("Prediction over test data: \n", predicted)
+
+plt.scatter(x=df['X'], y=df['Y'])
+plt.ylabel('Response')
+plt.xlabel('Explanatory')
 plt.plot(testX, predicted)
 plt.show()
