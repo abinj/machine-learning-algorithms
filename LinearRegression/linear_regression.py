397b3b8681410f14642c8675e931b3fecacf4662
abinj <abinmj656@gmail.com>
2019-09-30 15:11:26 -0400

    Dataset file changed, rmsd added as output

--- LinearRegression/linear_regression.py
+++ LinearRegression/linear_regression.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv("dataset.csv")
+df = pd.read_csv("grey_kangaroos.csv")
 df.columns = ['X', 'Y']
 
 trainX = np.asarray(df.X[20:len(df.X)]).reshape(-1, 1)
@@ -26,13 +26,14 @@ r2_value = model.score(trainX, trainY)
 rmsd = np.sqrt(mean_squared_error(testY, predicted))
 
 # Printing values
-print('Slope: \n', model.coef_)
+print("Slope: \n", model.coef_)
+print("Intercept: \n", model.intercept_)
 print("Root Mean Square Deviation: \n", rmsd)
 print("R^2 value: \n", r2_value)
 print("Prediction over test data: \n", predicted)
 
 plt.scatter(x=df['X'], y=df['Y'])
-plt.ylabel('Response')
-plt.xlabel('Explanatory')
+plt.ylabel('nasal width (mm ¥ 10)')
+plt.xlabel('nasal length (mm ¥10)')
 plt.plot(testX, predicted)
 plt.show()
