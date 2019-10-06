8b88dc71b8fba69819fce35fdad8ceb9032f4e48
abinj <abinmj656@gmail.com>
2019-10-06 15:12:51 -0400

    Multiple linear regression, code formatted

--- LinearRegression/multiple_linear_regression.py
+++ LinearRegression/multiple_linear_regression.py
@@ -5,7 +5,6 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
-import seaborn as seabornInstance
 
 
 df = pd.read_csv("dataset/major_league_baseball_players.csv")
