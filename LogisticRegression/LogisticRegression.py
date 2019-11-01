f19ca5310791b9542e0f222e3bffa7aaa55b6446
abinj <abinmj656@gmail.com>
2019-11-01 15:52:40 -0400

    Logistic regression updated

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 
 # Import the dataset
+from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from matplotlib.colors import ListedColormap
@@ -16,41 +18,44 @@ X = dataset.iloc[:, [2,3]].values
 Y = dataset.iloc[:, 4].values
 
 # Split the dataset into training and test set
-X_train, X_test, y_train, y_test  = train_test_split(X, Y, test_size = 0.25, random_state=0)
+X_train, X_test, y_train, y_test  = train_test_split(X, Y, test_size = 0.20, random_state=0)
 
 # Feature Scaling
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-classifier = LogisticRegression(random_state=0)
-classifier.fit(X_train, y_train)
+logreg = LogisticRegression(random_state=0)
+# Fit the model with data
+logreg.fit(X_train, y_train)
 
-y_pred = classifier.predict(X_test)
+y_pred = logreg.predict(X_test)
 
 cm = confusion_matrix(y_test, y_pred)
 print('Confusion matrix', cm)
 
-X1, X2 = np.meshgrid(np.arange(start = X_train[:, 0].min() - 1, stop = X_train[:, 0].max() + 1, step = 0.01),
-                     (np.arange(start = X_train[:, 1].min() -1, stop = X_train[:, 1].max() + 1, step = 0.01)))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
+class_names = [0, 1]
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
 
-for i,j in enumerate(np.unique(y_train)):
-    plt.scatter(X_train[y_train == j, 0], X_train[y_train == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
+sns.heatmap(pd.DataFrame(cm), annot=True, cmap = "YlGnBu", fmt = 'g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual Label')
+plt.xlabel('Predicted label')
 plt.show()
+print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
+report = classification_report(y_test, y_pred)
+print("Classification Report: ", report)
 
 X1, X2 = np.meshgrid(np.arange(start = X_test[:, 0].min() - 1, stop = X_test[:, 0].max() + 1, step = 0.01),
                      (np.arange(start = X_test[:, 1].min() -1, stop = X_test[:, 1].max() + 1, step = 0.01)))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.contourf(X1, X2, logreg.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.50, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_test)):
