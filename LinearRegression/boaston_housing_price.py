0c586cebba65a086758cdf95b48dd82b63136ae8
abinj <abinmj656@gmail.com>
2020-01-01 23:56:42 -0500

    Explonatory data analysis updating

--- LinearRegression/boaston_housing_price.py
+++ LinearRegression/boaston_housing_price.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.datasets import load_boston
+import seaborn as sns
 
 boston_dataset = load_boston()
 
@@ -11,3 +13,25 @@ boston['MEDV'] = boston_dataset.target
 
 boston.isnull().sum()
 
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+sns.distplot(boston['MEDV'], bins=30)
+plt.show()
+
+correlation_matrix = boston.corr().round(2)
+# annot = True to print the values inside the square
+sns.heatmap(data=correlation_matrix, annot=True)
+
+
+plt.figure(figsize=(20, 5))
+
+features = ['LSTAT', 'RM']
+target = boston['MEDV']
+
+for i, col in enumerate(features):
+    plt.subplot(1, len(features) , i+1)
+    x = boston[col]
+    y = target
+    plt.scatter(x, y, marker='o')
+    plt.title(col)
+    plt.xlabel(col)
+    plt.ylabel('MEDV')
