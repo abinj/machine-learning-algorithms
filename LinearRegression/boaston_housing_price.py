cc95eb7b3560b77073ebf79bb3ceb7904607c4bb
abinj <abinmj656@gmail.com>
2020-01-02 16:57:17 -0500

    Boaston house pricing evaluation done

--- LinearRegression/boaston_housing_price.py
+++ LinearRegression/boaston_housing_price.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.datasets import load_boston
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
 boston_dataset = load_boston()
 
@@ -26,6 +30,7 @@ plt.figure(figsize=(20, 5))
 
 features = ['LSTAT', 'RM']
 target = boston['MEDV']
+print(boston['MEDV'].count())
 
 for i, col in enumerate(features):
     plt.subplot(1, len(features) , i+1)
@@ -35,4 +40,35 @@ for i, col in enumerate(features):
     plt.title(col)
     plt.xlabel(col)
     plt.ylabel('MEDV')
-    plt.show()
+
+plt.show()
+
+X = pd.DataFrame(np.c_[boston['LSTAT'], boston['RM']], columns=['LSTAT', 'RM'])
+Y = boston['MEDV']
+
+train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.2, random_state=5)
+print(train_x.shape)
+print(test_x.shape)
+print(train_y.shape)
+print(test_y.shape)
+
+model = LinearRegression()
+model.fit(train_x, train_y)
+
+#Model Evaluation
+predict = model.predict(train_x)
+rmse = (np.sqrt(mean_squared_error(train_y, predict)))
+r2 = r2_score(train_y, predict)
+
+print("The model performance for training set")
+print("RMSE is: {}".format(rmse))
+print("R2 value is {}".format(r2))
+
+print("Model performace for testing set")
+test_predict = model.predict(test_x)
+rmse = (np.sqrt(mean_squared_error(test_y, test_predict)))
+r2 = r2_score(test_y, test_predict)
+
+print("Test set RMSE is: {}".format(rmse))
+print("Test set r2 value is: {}".format(r2))
+
